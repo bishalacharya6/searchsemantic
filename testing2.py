6fc407315849231eadff7734a87715457358de13
bishalacharya6 <bishalacharya673@gmail.com>
2024-04-03 16:15:26 +0545

    asdfs

--- testing2.py
+++
@@ -1,6 +0,0 @@
-import pycuda.driver as cuda
-cuda.init()
-device = cuda.Device(0)  # Assuming GPU 0
-num_cores = device.get_attribute(cuda.device_attribute.MULTIPROCESSOR_COUNT) * \
-            cuda.Device(0).get_attribute(cuda.device_attribute.MULTIPROCESSOR_CORE_COUNT)
-print("Number of CUDA Cores:", num_cores)
